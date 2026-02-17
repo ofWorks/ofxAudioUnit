53d093f46dfa82b6154ac5a34c47b56f32beea25
Dimitre <dimitre@users.noreply.github.com>
2026-02-17 00:25:43 -0300

    fixes cleanup

--- src/ofxAudioUnitCocoaUtilties.mm
+++ src/ofxAudioUnitCocoaUtilties.mm
@@ -7,24 +7,26 @@
 
 #pragma mark Objective-C
 
-// Keep track of open windows to prevent premature release
-static NSMutableSet *gOpenWindows = nil;
+// Forward declaration for tracking functions
+@interface ofxAudioUnitUIWindow : NSWindow
+@property (nonatomic, strong) NSView *auView;
+- (instancetype)initWithAudioUnit:(AudioUnit)unit forceGeneric:(BOOL)useGeneric;
+@end
 
-static NSMutableSet* GetOpenWindows() {
+// Static set to keep strong references to open windows (prevents premature release under ARC)
+static NSMutableSet *gOpenWindows;
+
+static void TrackWindow(ofxAudioUnitUIWindow *window) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         gOpenWindows = [[NSMutableSet alloc] init];
     });
-    return gOpenWindows;
+    [gOpenWindows addObject:window];
 }
 
-@interface ofxAudioUnitUIWindow : NSWindow
-
-@property (nonatomic, strong) NSView *auView;
-
-- (instancetype)initWithAudioUnit:(AudioUnit)unit forceGeneric:(BOOL)useGeneric;
-
-@end
+static void UntrackWindow(ofxAudioUnitUIWindow *window) {
+    [gOpenWindows removeObject:window];
+}
 
 @implementation ofxAudioUnitUIWindow
 
@@ -32,14 +34,7 @@ static NSMutableSet* GetOpenWindows() {
     [[NSNotificationCenter defaultCenter] removeObserver:self
                                                     name:NSViewFrameDidChangeNotification
                                                   object:self.auView];
-
-    // Remove from tracking set
-    [[GetOpenWindows() objectsPassingTest:^BOOL(id obj, BOOL *stop) {
-        return obj == self;
-    }] enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
-        [GetOpenWindows() removeObject:obj];
-    }];
-
+    UntrackWindow(self);
 #if !__has_feature(objc_arc)
     [_auView release];
     [super dealloc];
@@ -52,28 +47,20 @@ static NSMutableSet* GetOpenWindows() {
     if (useGeneric) {
         view = [self createGenericViewForUnit:unit];
     } else if ([ofxAudioUnitUIWindow audioUnitHasCocoaView:unit]) {
-        view = [self createCocoaViewForUnit:unit];
-        if (!view) {
-            view = [self createGenericViewForUnit:unit];
-        }
+        view = [self createCocoaViewForUnit:unit] ?: [self createGenericViewForUnit:unit];
     } else if ([ofxAudioUnitUIWindow audioUnitHasCarbonView:unit]) {
-        [self printUnsupportedCarbonMessage:unit];
+        NSLog(@"This audio unit only has a Carbon-based UI. Carbon support has been removed.");
         return nil;
     } else {
         view = [self createGenericViewForUnit:unit];
     }
 
-    if (!view) {
-        return nil;
-    }
+    if (!view) return nil;
 
     self = [self initWithAudioUnitView:view];
-
-    // Add to tracking set to keep a strong reference
     if (self) {
-        [GetOpenWindows() addObject:self];
+        TrackWindow(self);
     }
-
     return self;
 }
 
@@ -89,51 +76,28 @@ static NSMutableSet* GetOpenWindows() {
 - (NSView *)createCocoaViewForUnit:(AudioUnit)unit {
     UInt32 dataSize;
     Boolean isWriteable;
-    OSStatus result = AudioUnitGetPropertyInfo(unit,
-                                               kAudioUnitProperty_CocoaUI,
-                                               kAudioUnitScope_Global,
-                                               0,
-                                               &dataSize,
-                                               &isWriteable);
-
-    if (result != noErr) {
+    if (AudioUnitGetPropertyInfo(unit, kAudioUnitProperty_CocoaUI, kAudioUnitScope_Global,
+                                  0, &dataSize, &isWriteable) != noErr) {
         return nil;
     }
 
     UInt32 numberOfClasses = (dataSize - sizeof(CFURLRef)) / sizeof(CFStringRef);
-    if (numberOfClasses == 0) {
-        return nil;
-    }
+    if (numberOfClasses == 0) return nil;
 
     AudioUnitCocoaViewInfo *cocoaViewInfo = (AudioUnitCocoaViewInfo *)malloc(dataSize);
-    if (!cocoaViewInfo) {
-        return nil;
-    }
+    if (!cocoaViewInfo) return nil;
 
     NSView *view = nil;
-    OSStatus success = AudioUnitGetProperty(unit,
-                                            kAudioUnitProperty_CocoaUI,
-                                            kAudioUnitScope_Global,
-                                            0,
-                                            cocoaViewInfo,
-                                            &dataSize);
-
-    if (success == noErr) {
-        CFURLRef bundlePath = cocoaViewInfo->mCocoaAUViewBundleLocation;
-        CFStringRef className = cocoaViewInfo->mCocoaAUViewClass[0];
-        NSBundle *bundle = [NSBundle bundleWithURL:(__bridge NSURL *)bundlePath];
-
-        if (bundle) {
-            Class factoryClass = [bundle classNamed:(__bridge NSString *)className];
-            if (factoryClass) {
-                id<AUCocoaUIBase> factory = [[factoryClass alloc] init];
-                if (factory) {
-                    view = [factory uiViewForAudioUnit:unit withSize:NSZeroSize];
+    if (AudioUnitGetProperty(unit, kAudioUnitProperty_CocoaUI, kAudioUnitScope_Global,
+                              0, cocoaViewInfo, &dataSize) == noErr) {
+        NSBundle *bundle = [NSBundle bundleWithURL:(__bridge NSURL *)cocoaViewInfo->mCocoaAUViewBundleLocation];
+        Class factoryClass = [bundle classNamed:(__bridge NSString *)cocoaViewInfo->mCocoaAUViewClass[0]];
+        if (factoryClass) {
+            id<AUCocoaUIBase> factory = [[factoryClass alloc] init];
+            view = [factory uiViewForAudioUnit:unit withSize:NSZeroSize];
 #if !__has_feature(objc_arc)
-                    [factory release];
+            [factory release];
 #endif
-                }
-            }
         }
     }
 
@@ -143,7 +107,6 @@ static NSMutableSet* GetOpenWindows() {
 
 - (instancetype)initWithAudioUnitView:(NSView *)view {
     NSRect contentRect = NSMakeRect(0, 0, view.frame.size.width, view.frame.size.height);
-
     self = [super initWithContentRect:contentRect
                             styleMask:(NSWindowStyleMaskTitled |
                                        NSWindowStyleMaskClosable |
@@ -154,14 +117,13 @@ static NSMutableSet* GetOpenWindows() {
     if (self) {
         self.auView = view;
         self.contentView = view;
-        self.releasedWhenClosed = NO;  // Don't release when closed, we'll manage it
+        self.releasedWhenClosed = NO;  // Critical: prevents crash on window close
 
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(viewFrameChanged:)
                                                      name:NSViewFrameDidChangeNotification
                                                    object:view];
     }
-
     return self;
 }
 
@@ -185,22 +147,12 @@ static NSMutableSet* GetOpenWindows() {
                                                object:view];
 }
 
-- (void)printUnsupportedCarbonMessage:(AudioUnit)unit {
-    NSLog(@"This audio unit only has a Carbon-based UI. Carbon support has been removed from ofxAudioUnit.");
-}
-
 + (BOOL)audioUnitHasCocoaView:(AudioUnit)unit {
     UInt32 dataSize;
     Boolean isWriteable;
-    OSStatus result = AudioUnitGetPropertyInfo(unit,
-                                               kAudioUnitProperty_CocoaUI,
-                                               kAudioUnitScope_Global,
-                                               0,
-                                               &dataSize,
-                                               &isWriteable);
-
+    OSStatus result = AudioUnitGetPropertyInfo(unit, kAudioUnitProperty_CocoaUI,
+                                               kAudioUnitScope_Global, 0, &dataSize, &isWriteable);
     if (result != noErr) return NO;
-
     UInt32 numberOfClasses = (dataSize - sizeof(CFURLRef)) / sizeof(CFStringRef);
     return numberOfClasses > 0;
 }
@@ -208,13 +160,8 @@ static NSMutableSet* GetOpenWindows() {
 + (BOOL)audioUnitHasCarbonView:(AudioUnit)unit {
     UInt32 dataSize;
     Boolean isWriteable;
-    OSStatus result = AudioUnitGetPropertyInfo(unit,
-                                               kAudioUnitProperty_GetUIComponentList,
-                                               kAudioUnitScope_Global,
-                                               0,
-                                               &dataSize,
-                                               &isWriteable);
-
+    OSStatus result = AudioUnitGetPropertyInfo(unit, kAudioUnitProperty_GetUIComponentList,
+                                               kAudioUnitScope_Global, 0, &dataSize, &isWriteable);
     return (result == noErr) && (dataSize >= sizeof(ComponentDescription));
 }
 
@@ -222,13 +169,11 @@ static NSMutableSet* GetOpenWindows() {
 
 #pragma mark - C++
 
-using namespace std;
-
-void ofxAudioUnit::showUI(const string &title, int x, int y, bool forceGeneric) {
+void ofxAudioUnit::showUI(const std::string &title, int x, int y, bool forceGeneric) {
     if (!_unit.get()) return;
 
     AudioUnitRef auRef = _unit;
-    string titleCopy = title;
+    std::string titleCopy = title;
 
     dispatch_async(dispatch_get_main_queue(), ^{
         if (!auRef) return;
@@ -239,9 +184,7 @@ void ofxAudioUnit::showUI(const string &title, int x, int y, bool forceGeneric) 
 
         CGFloat flippedY = [[NSScreen mainScreen] visibleFrame].size.height - y - window.frame.size.height;
         [window setFrameOrigin:NSMakePoint(x, flippedY)];
-
-        NSString *windowTitle = [NSString stringWithUTF8String:titleCopy.c_str()];
-        [window setTitle:windowTitle ?: @"Audio Unit UI"];
+        [window setTitle:[NSString stringWithUTF8String:titleCopy.c_str()] ?: @"Audio Unit UI"];
         [window makeKeyAndOrderFront:nil];
 
 #if !__has_feature(objc_arc)
